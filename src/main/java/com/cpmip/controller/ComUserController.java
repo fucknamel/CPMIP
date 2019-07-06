1ef1c91f42a4e21e382ecd0de0568ca5827ceef8
fucknamel <1047760450@qq.com>
2019-07-06 21:14:31 +0800

    get_user_info finished

--- src/main/java/com/cpmip/controller/ComUserController.java
+++ src/main/java/com/cpmip/controller/ComUserController.java
@@ -51,4 +51,14 @@ public class ComUserController {
     public ServerResponse<String> checkValid(String str, String type) {
         return iComUserService.checkValid(str, type);
     }
+
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse getUserInfo(HttpSession session) {
+        UserVo user = (UserVo) session.getAttribute(Const.CURRENT_USER);
+        if (user != null) {
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户信息");
+    }
 }
--- src/main/java/com/cpmip/controller/GovUserController.java
+++ src/main/java/com/cpmip/controller/GovUserController.java
@@ -51,4 +51,14 @@ public class GovUserController {
     public ServerResponse<String> checkValid(String str, String type) {
         return iGovUserService.checkValid(str, type);
     }
+
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse getUserInfo(HttpSession session) {
+        UserVo user = (UserVo) session.getAttribute(Const.CURRENT_USER);
+        if (user != null) {
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByErrorMessage("用户未登录，无法获取当前用户信息");
+    }
 }
