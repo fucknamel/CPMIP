e92516f5c3381f9be64a05e8792edb0298b45409
fucknamel <1047760450@qq.com>
2019-07-12 08:44:57 +0800

    comp add.do updated

--- src/main/java/com/cpmip/controller/CompController.java
+++ src/main/java/com/cpmip/controller/CompController.java
@@ -22,10 +22,7 @@ public class CompController {
     @RequestMapping(value = "add.do", method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse add(Complaints item, HttpSession session){
-        if (session.getAttribute(Const.CURRENT_USER) != null) {
-            return iCompService.add(item);
-        }
-        return ServerResponse.createByErrorMessage("用户未登录");
+        return iCompService.add(item);
     }
 
     @RequestMapping(value = "delete.do", method = RequestMethod.POST)
